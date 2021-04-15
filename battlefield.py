d35b4e64d660519028039ad48cab41aa3ff5657a
unknown <mpeterson2004@gmail.com>
2021-04-15 12:41:04 -0600

    Created battlefield class /w all required methods(currently empty)

---
+++ battlefield.py
@@ -0,0 +1,25 @@
+from herd import Herd
+from robotFleet import Fleet
+
+
+class Battlefield:
+    def __init__(self):
+        self.fleet = Fleet()
+        self.herd = Herd()
+
+    def run_game(self):
+
+    def display_welcome(self):
+
+    def battle(self):
+
+    def dino_turn(self, dinosaur):
+
+    def robo_turn(self, robot):
+
+    def show_dino_opponent_options(self):
+
+    def show_robo_opponent_options(self):
+
+    def display_winners(self):
+        
