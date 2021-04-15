7f93fa3f0baaa048598b595abb404fe0a4644add
unknown <mpeterson2004@gmail.com>
2021-04-15 09:54:35 -0600

    created instance of robot fleet and added method to check the current health of the RobotFleet

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from robot import Robot
 from robotFleet import RobotFleet
 from weapon import Weapon
 def test_method():
-    print("put stuff here")
+    rob = Robot(Weapon("Missle_Pod"), "R.O.B")
+    data = Robot(Weapon("Rifle"), "Data")
+    twob = Robot(Weapon("Sword"), "2B")
+    machine_overlord = RobotFleet([rob, data, twob])
+
 
 
 if __name__ == '__main__':
--- robotFleet.py
+++ robotFleet.py
@@ -1,5 +1,11 @@
 class RobotFleet:
     def __init__(self, robots=[]):
         self.robots = robots
+        self.health = 0
+        for x in robots:
+            self.health += x.life
     def recruitment(self, robot):
         self.robots.append(robot)
+
+    def check_health(self):
+        return self.health
